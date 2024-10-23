5ca7db45b0cd2f34c39c26c636f4f530d57eea80
iamhyedddi <iamhyedddi@gmail.com>
2024-10-23 21:54:13 +0900

    todolist01

--- vvue/src/components/TodoCheckList.vue
+++ vvue/src/components/TodoCheckList.vue
@@ -1,42 +1,92 @@
 <template>
-  <p><button type="button">+</button></p>
-  <ul>
-    <li>
-      <span class="checkbox-wrap">
-        <input
-          type="checkbox"
-          name=""
-          class="checkbox-input"
-          id="checkList00"
-        />
-        <label for="checkList00" class="checkbox-label">ㅇㅇㅇㅇㅇㅇ</label>
-      </span>
-    </li>
-  </ul>
+  <div class="list-wrap">
+    <ul>
+      <li>
+        <span class="checkbox-wrap">
+          <input
+            type="checkbox"
+            name=""
+            class="checkbox-input"
+            id="checkList00"
+          />
+        </span>
+        <span class="inputTxt-wrap">
+          <input type="text" name="" id="" placeholder="간지나게 숨쉬기" />
+        </span>
+      </li>
+    </ul>
+    <div class="add-btn-wrap">
+      <button type="button" class="add-list" @click="add - list">+</button>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+function add() {
+  alert('...');
+}
+</script>
 
 <style lang="scss" scoped>
-@import '../assets/_variables.scss';
-@import '../assets/_mixin.scss';
+.list-wrap {
+  padding: 1.6rem 1rem;
+}
+
+.add-btn-wrap {
+  margin: 1.6rem 0 0 0;
+  text-align: right;
+}
+
+.add-list {
+  display: block;
+  width: 100%;
+  padding: 0.5rem 1rem;
+  background: #ffffff;
+  border: none;
+  border-radius: 1rem;
+  font-size: 2rem;
+}
+
 ul {
-  @include flex-row(10px);
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
   li {
+    display: flex;
   }
 }
 
 .checkbox-wrap {
-  display: $display-ib;
-}
-.checkbox-input {
-  display: none;
+  display: block;
 }
 
-.checkbox-label {
+.checkbox-wrap input {
   display: block;
-  padding: 0 0 0 2.5rem;
-  line-height: 20px;
-  font-size: 1.8rem;
+  width: 2rem;
+  height: 2rem;
+  border-color: #ccc;
+  outline-color: #ccc;
+}
+
+.inputTxt-wrap {
+  width: calc(100% - 2rem);
+}
+
+.inputTxt-wrap input {
+  display: block;
+  width: 100%;
+  padding: 0 0 0 1rem;
+  line-height: 2rem;
+  font-size: 1.6rem;
+}
+
+.inputTxt-wrap input:focus {
+  border: none;
+  outline: none;
+}
+
+.inputTxt-wrap input::placeholder {
+  color: #ccc;
 }
 </style>
